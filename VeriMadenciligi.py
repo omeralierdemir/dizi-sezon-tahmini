60b3937e6464965db6fe6f7e72b41dceaf570dbd
omer ali <o.erdemir1997@gmail.com>
2018-12-30 02:15:20 +0300

    knn dosyasi eklendi

--- VeriMadenciligi.py
+++ VeriMadenciligi.py
@@ -3,14 +3,14 @@ import requests
 import VeriTemizleme as vt
 
 
-url = 'https://www.imdb.com/search/title?title_type=tv_series&countries=tr&ref_=adv_prv'
-url2 = "https://www.imdb.com/search/title?title_type=tv_series&countries=tr&sort=year,asc"
-url3 = "https://www.imdb.com/search/title?title_type=tv_series&countries=tr&sort=year,asc"
-url4 = "https://www.imdb.com/search/title?title_type=tv_series&countries=tr&sort=year,asc&start=51&ref_=adv_nxt"
-reps = requests.get(url)
-soup = BeautifulSoup(reps.content,'html.parser')
-
-contentVerileri = soup.find_all('div',{"class":"lister-item-content"})
+# url = 'https://www.imdb.com/search/title?title_type=tv_series&countries=tr&ref_=adv_prv'
+# url2 = "https://www.imdb.com/search/title?title_type=tv_series&countries=tr&sort=year,asc"
+# url3 = "https://www.imdb.com/search/title?title_type=tv_series&countries=tr&sort=year,asc"
+# url4 = "https://www.imdb.com/search/title?title_type=tv_series&countries=tr&sort=year,asc&start=51&ref_=adv_nxt"
+# reps = requests.get(url)
+# soup = BeautifulSoup(reps.content,'html.parser')
+#
+# contentVerileri = soup.find_all('div',{"class":"lister-item-content"})
 
 counter = 0
 
@@ -22,37 +22,51 @@ turT = []
 imdbRange = []
 actor = []
 kopru = []
-for i in contentVerileri:
 
-    diziIsimleri.append(i.find("h3").find('a').text)
-    tarihVerileri.append(i.find("h3").find('span',{"class":"lister-item-year text-muted unbold"}).text)
+for urlDeger in range(1,1301,50):
 
-    turT.append(i.find("p").find("span",{"class":"genre"}).text)
+    if (urlDeger == 1):
+        url = "https://www.imdb.com/search/title?title_type=tv_series&countries=tr&sort=year,asc&ref_=adv_prv"
 
-  #  print(i.find("p").find("span",{"class":"genre"}).text)
-    imdbRange.append(i.find("strong").text)
-    star = i.find_all("p")
-    stars = star[2].find_all("a")
-   # fatih = omer[0].text
+    else:
+        url = "https://www.imdb.com/search/title?title_type=tv_series&countries=tr&sort=year,asc&start=" + str(urlDeger) + "&ref_=adv_nxt"
 
-    #print(tarihVerileri)  # append kullanılacak
-    #print(star[2])
-  #q  print(tur)
+    reps = requests.get(url)
+    soup = BeautifulSoup(reps.content, 'html.parser')
+    contentVerileri = soup.find_all('div', {"class": "lister-item-content"})
+
+    for i in contentVerileri:
+
+        if((i.find("strong")) != None and (i.find("p").find("span",{"class":"genre"})) != None and  i.find_all("p")[2].find_all("a") != None  and (i.find("p").find("span",{"class":"genre"}))):
+            diziIsimleri.append(i.find("h3").find('a').text)
+            tarihVerileri.append(i.find("h3").find('span',{"class":"lister-item-year text-muted unbold"}).text)
+
+            turT.append(i.find("p").find("span",{"class":"genre"}).text)
+
+          #  print(i.find("p").find("span",{"class":"genre"}).text)
+            imdbRange.append(i.find("strong").text)
+            star = i.find_all("p")
+            stars = star[2].find_all("a")
+           # fatih = omer[0].text
+
+            #print(tarihVerileri)  # append kullanılacak
+            #print(star[2])
+          #q  print(tur)
 
 
 
-    # o1 = omer[0].text
-    for j in stars:
-        kopru.append(j.text)
+            # o1 = omer[0].text
+            for j in stars:
+                kopru.append(j.text)
 
-    actor.append(kopru)
-    kopru = []
+            actor.append(kopru)
+            kopru = []
 
 
 
 turT = vt.satirTemizle(turT)
 tur = vt.tur(turT)
-print(actor)
-print(turT)
-print(len(tur))
+print(diziIsimleri)
+#print(turT)
+print(len(diziIsimleri))
 print(type(tarihVerileri[0]))
